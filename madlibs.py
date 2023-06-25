2f2ea73b3d59e2f1ee44a2eb3e6df7a79d030eb9
Nikhila9921 <106167983+Nikhila9921@users.noreply.github.com>
2023-06-25 11:17:42 +0530

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,10 @@
+name = input("Name:  ")
+city = input("City:  ")
+current_studies = input("Current Studies:  ")
+number = int(input())
+hobbies=input("hobbies: ")
+family_type = input("family_type: ")
+
+
+madlib = f"Hello There, Iam {name}.I hail from {city}.Most of my life I stayed in {city} only.I live in {family_type} with {number} members.Coming to my studies Iam currently pursuing my {current_studies} My hobbies are {hobbies}."
+print(madlib)
